adc3007aa2a0d9e21c68b51e6fe19922ae19b23e
Pablo Tato Ramos <pabtatoramos@gmail.com>
2021-02-06 22:42:22 +0100

    Reformat code

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,12 @@ use winit_input_helper::WinitInputHelper;
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
-struct RGBA { r: u8, g: u8, b: u8, a: u8 }
+struct RGBA {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+}
 
 type PutPixelFn = extern "C" fn(*mut Canvas, i32, i32, RGBA);
 
@@ -18,8 +23,9 @@ struct Canvas {
     height: i32,
     put_pixel: PutPixelFn,
 }
-extern "C" { fn frame(canvas: *mut Canvas); }
-
+extern "C" {
+    fn frame(canvas: *mut Canvas);
+}
 
 extern "C" fn put_pixel(canvas: *mut Canvas, x: i32, y: i32, color: RGBA) {
     let canvas: &mut Canvas = unsafe { &mut (*canvas) };
@@ -27,24 +33,21 @@ extern "C" fn put_pixel(canvas: *mut Canvas, x: i32, y: i32, color: RGBA) {
     let y = canvas.height / 2 - y;
     let i = (y * canvas.width + x) as usize;
     unsafe {
-        canvas.memory.add(i*4).write(color.r);
-        canvas.memory.add(i*4 + 1).write(color.g);
-        canvas.memory.add(i*4 + 2).write(color.b);
-        canvas.memory.add(i*4 + 3).write(color.a);
+        canvas.memory.add(i * 4).write(color.r);
+        canvas.memory.add(i * 4 + 1).write(color.g);
+        canvas.memory.add(i * 4 + 2).write(color.b);
+        canvas.memory.add(i * 4 + 3).write(color.a);
     }
 }
 
-
 fn main() {
-    let width: i32 = 512;
-    let height: i32 = 512;
-
     let event_loop = EventLoop::new();
     let mut input = WinitInputHelper::new();
     let window = {
-        let size = LogicalSize::new(width as f64, height as f64);
+        let size = LogicalSize::new(512.0, 512.0);
         WindowBuilder::new()
-            .with_title("Hello Pixels")
+            .with_resizable(false)
+            .with_title("Raytracer")
             .with_inner_size(size)
             .with_min_inner_size(size)
             .build(&event_loop)
@@ -54,14 +57,27 @@ fn main() {
     let mut pixels = {
         let window_size = window.inner_size();
         let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(width as u32, height as u32, surface_texture).unwrap()
+        Pixels::new(
+            window_size.width as u32,
+            window_size.height as u32,
+            surface_texture,
+        )
+        .unwrap()
     };
 
     event_loop.run(move |event, _, control_flow| {
         if let Event::RedrawRequested(_) = event {
+            let window_size = window.inner_size();
             let buffer = pixels.get_frame();
-            let mut canvas = Canvas { memory: buffer.as_mut_ptr(), width, height, put_pixel };
-            unsafe { frame(&mut canvas); };
+            let mut canvas = Canvas {
+                memory: buffer.as_mut_ptr(),
+                width: window_size.width as i32,
+                height: window_size.height as i32,
+                put_pixel,
+            };
+            unsafe {
+                frame(&mut canvas);
+            };
 
             if pixels.render().is_err() {
                 *control_flow = ControlFlow::Exit;
